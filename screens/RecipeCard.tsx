4099b7b873d62da5597ff62a793b052495a8e512
Andres.Arana <andres.arana@jala-foundation.org>
2021-07-28 14:51:26 -0400

    Modify card styles

--- screens/RecipeCard.tsx
+++ screens/RecipeCard.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { Card } from 'react-native-elements';
 import { Dimensions } from 'react-native';
 
 const RecipeCard = () => {
     return (
         <Card containerStyle={styles.card}>
-            <Image
+            <Card.Image
                 style={styles.image}
                 source={{ uri: 'https://www.krumpli.co.uk/wp-content/uploads/2019/12/Lasagna-Bolognese-4.jpg.webp' }}>
-            </Image>
+            </Card.Image>
+            <Card.Divider />
+            <Card.FeaturedTitle style={styles.plateName}>Lasagne</Card.FeaturedTitle>
+            <Card.FeaturedSubtitle style={styles.plateName}>60 min</Card.FeaturedSubtitle>
         </Card>
     )
 }
@@ -18,15 +21,17 @@ export default RecipeCard
 
 const styles = StyleSheet.create({
     card: {
-        height: Dimensions.get('window').height * 0.60,
         width: Dimensions.get('window').width * 0.80,
-        borderRadius: 20,
+        borderRadius: 10,
         padding: 0,
-        borderColor: '#FFFFFF',
     },
     image: {
-        height: '100%',
-        width: '100%',
-        borderRadius: 20,
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        height: Dimensions.get('window').height * 0.60,
+        marginBottom: 10,
+    },
+    plateName: {
+        color: 'black'
     },
 })
