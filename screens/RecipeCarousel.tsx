3e40d1f8c3cde8e4b9609b937e709080dc8b4e6b
Andres.Arana <andres.arana@jala-foundation.org>
2021-07-29 15:06:33 -0400

    Display carousel as in design

--- screens/RecipeCarousel.tsx
+++ screens/RecipeCarousel.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, Dimensions } from 'react-native'
+import { Text, StyleSheet, Dimensions, View } from 'react-native'
 import RecipeCard from './RecipeCard';
 import Carousel from 'react-native-snap-carousel';
 import { RECIPES } from '../static/recipes';
@@ -8,21 +8,37 @@ class RecipeCarousel extends Component {
 
     render() {
         return (
-            <>
+            <View style={styles.container}>
                 <Text>Recipes</Text>
                 {/* <RecipeCard /> */}
-                <Carousel
-                    data={RECIPES}
-                    renderItem={RecipeCard}
-                    sliderWidth={Dimensions.get('window').width}
-                    itemWidth={Dimensions.get('window').width * 0.80}
-                    useScrollView={true}
-                />
-            </>
+                <View style={styles.carouselContainer}>
+                    <Carousel
+                        data={RECIPES}
+                        renderItem={RecipeCard}
+                        sliderWidth={Dimensions.get('window').width}
+                        itemWidth={Dimensions.get('window').width * 0.80}
+                        contentContainerCustomStyle={styles.carousel}
+                    />
+                </View>
+                <Text>Filters</Text>
+            </ View>
         )
     }
 }
 
 export default RecipeCarousel
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    carouselContainer: {
+        height: Dimensions.get('window').height * 0.70,
+    },
+    carousel: {
+        paddingLeft: 10,
+    }
+})
--- static/recipes.ts
+++ static/recipes.ts
@@ -14,4 +14,19 @@ export const RECIPES = [
         description: 'Lasagne are a type of wide, flat pasta, possibly one of the oldest types of pasta. Lasagne, or the singular lasagna, is an Italian dish made of stacked layers of thin flat pasta alternating with fillings such as ragù and other vegetables, cheese, and seasonings and spices such as garlic, oregano and basil.',
         time: '1h 40min'
     },
+    {
+        title: 'Lasagne',
+        description: 'Lasagne are a type of wide, flat pasta, possibly one of the oldest types of pasta. Lasagne, or the singular lasagna, is an Italian dish made of stacked layers of thin flat pasta alternating with fillings such as ragù and other vegetables, cheese, and seasonings and spices such as garlic, oregano and basil.',
+        time: '1h 40min'
+    },
+    {
+        title: 'Lasagne 2',
+        description: 'Lasagne are a type of wide, flat pasta, possibly one of the oldest types of pasta. Lasagne, or the singular lasagna, is an Italian dish made of stacked layers of thin flat pasta alternating with fillings such as ragù and other vegetables, cheese, and seasonings and spices such as garlic, oregano and basil.',
+        time: '1h 40min'
+    },
+    {
+        title: 'Lasagne 3',
+        description: 'Lasagne are a type of wide, flat pasta, possibly one of the oldest types of pasta. Lasagne, or the singular lasagna, is an Italian dish made of stacked layers of thin flat pasta alternating with fillings such as ragù and other vegetables, cheese, and seasonings and spices such as garlic, oregano and basil.',
+        time: '1h 40min'
+    },
 ];
