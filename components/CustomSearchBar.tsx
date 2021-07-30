dfc1f5d059030c3243a8e0a0d977a87de18c2a07
Andres.Arana <andres.arana@jala-foundation.org>
2021-07-30 14:39:26 -0400

    Style custom search bar

--- components/CustomSearchBar.tsx
+++ components/CustomSearchBar.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Dimensions, StyleSheet, TextInput } from 'react-native'
+import { Dimensions, StyleSheet, TextInput, View } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
 
 type SearchBarProps = {
     onChangeText: (search: string) => void
@@ -27,25 +28,40 @@ export default class CustomSearchBar extends Component<SearchBarProps, SearchBar
         }
 
         return (
-            <TextInput
-                style={styles.searchBar}
-                onChangeText={onUpdateText}
-                value={search}
-                placeholder="Search recipes..."
-                keyboardType="default"
-            />
+            <View style={styles.searchSection}>
+                <AntDesign name="search1" style={styles.searchIcon} size={24} color="black" />
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onUpdateText}
+                    value={search}
+                    placeholder="Search recipes..."
+                    keyboardType="default"
+                />
+            </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    searchBar: {
-        width: Dimensions.get('window').width * 0.90,
+    searchSection: {
+        width: Dimensions.get('window').width * 0.80,
         height: 40,
-        backgroundColor: 'white',
         borderRadius: 20,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    searchIcon: {
+        padding: 10,
     },
     input: {
-
+        paddingTop: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        paddingLeft: 0,
+        backgroundColor: 'white',
+        color: '#424242',
+        fontSize: 16,
     },
 })
