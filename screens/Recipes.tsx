ae2e45c9a0338f17d34068b966c632e069b8fab7
Andres.Arana <andres.arana@jala-foundation.org>
2021-08-12 18:28:00 -0400

    remove unnecessary imports

--- screens/Recipes.tsx
+++ screens/Recipes.tsx
@@ -1,24 +1,14 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
 import { StackNavigationProp } from '@react-navigation/stack';
 import routes from '../navigation/routes';
 import { RecipeCarousel, CustomSearchBar, FiltersBar, FiltersBottomSheet } from '../components';
-import RecipesService from '../services/RecipesService';
-import RecipesStorage from '../services/RecipesStorage';
-import { FILTERS } from '../static';
 import { COLORS } from '../styles';
 import { Recipe } from '../models';
-import { useState } from 'react';
-
-type RecipesState = {
-    displayFiltersSheet: boolean,
-}
 
 type RecipesProps = {
     navigation: StackNavigationProp<any>,
 }
-
-
 
 const Recipes: React.FC<RecipesProps> = ({
     navigation,
